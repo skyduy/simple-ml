8df3d2c3f462f5116ccb35baf2902131d1daa5c3
YuJun <skyduy@outlook.com>
2019-12-19 20:47:42 +0800

    Update apriori.py

--- frequent_itemset/apriori.py
+++ frequent_itemset/apriori.py
@@ -2,106 +2,75 @@
 """
 先验算法
 """
+import collections
 
 
 def load_data():
-    return [
-        [1, 3, 4],
-        [2, 3, 5],
-        [1, 2, 3, 5],
-        [2, 5],
+    data_set = [
+        [1, 2, 3, 4],
+        [1, 2, 4, 5],
+        [1, 3, 4, 5],
     ]
+    return {frozenset(i) for i in data_set}
 
 
-def create_items(data):
-    items = []
-    for transaction in data:
-        for item in transaction:
-            if [item] not in items:
-                items.append([item])
-    items.sort()
-    return map(frozenset, items)
+class Apriori:
+    def __init__(self, data=None):
+        if data is None:
+            self.data = load_data()
+        else:
+            self.data = data
+        self.num_items = len(self.data)
+        self.one_itemset = {frozenset({i}) for transaction in self.data for i in transaction}
 
+    def filter_supported(self, candidates, min_support):
+        candidate_cnt = collections.Counter()
+        for each in self.data:
+            for item in candidates:
+                if item.issubset(each):
+                    candidate_cnt[item] += 1
 
-def get_strong_data(data, candidates, min_support):
-    candidate_cnt = {}
-    for one in data:
-        for item in candidates:
-            if item.issubset(one):
-                candidate_cnt.setdefault(item, 0)
-                candidate_cnt[item] += 1
-    num_items = float(len(data))
-    rest_list = []
-    support_data = {}
-    for key in candidate_cnt:
-        support = candidate_cnt[key]/num_items
-        if support >= min_support:
-            rest_list.insert(0, key)
-        support_data[key] = support
-    return rest_list, support_data
-
-
-def generate_posterity(strong_data, size):
-    rest_list = []
-    length = len(strong_data)
-    for i in range(length):
-        for j in range(i+1, length):
-            list1 = list(strong_data[i])[:size-2]
-            list2 = list(strong_data[j])[:size-2]
-            list1.sort()
-            list2.sort()
-            if list1 == list2:
-                rest_list.append(strong_data[i] | strong_data[j])
-    return rest_list
-
-
-def apriori(data_set, min_support=0.5):
-    items = create_items(data_set)
-    data = map(set, data_set)
-    strong_data, support_data = get_strong_data(data, items, min_support)
-    all_data = [strong_data]
-    size = 2
-    while len(all_data[size-2]) > 0:
-        posterity = generate_posterity(all_data[size-2], size)
-        new_strong_data, new_support_data = get_strong_data(data, posterity, min_support)
-        support_data.update(new_support_data)
-        all_data.append(new_strong_data)
-        size += 1
-    return all_data, support_data
-
-
-def generate_rules(all_data, support_data, min_conf):
-    big_rule_list = []
-    for i in range(1, len(all_data)):
-        for freq_set in all_data[i]:
-            h1 = [frozenset([item]) for item in freq_set]
-            if i > 1:
-                rules_from_set_item(freq_set, h1, support_data, big_rule_list, min_conf)
+        pivot = min_support * self.num_items
+        prompted, kicked = [], []
+        for itemset, count in candidate_cnt.items():
+            if count >= pivot:
+                prompted.append(itemset)
             else:
-                calc_conf(freq_set, h1, support_data, big_rule_list, min_conf)
-    return big_rule_list
+                kicked.append(itemset)
 
+        return prompted, kicked
 
-def calc_conf(freq_set, h, support_data, br1, min_conf=0.7):
-    pruned_h = []
-    for set_item in h:
-        conf = support_data[freq_set]/support_data[freq_set-set_item]
-        if conf >= min_conf:
-            print freq_set-set_item, '-->', set_item, 'conf:', conf
-            br1.append((freq_set-set_item, set_item, conf))
-            pruned_h.append(set_item)
-    return pruned_h
+    def generate_next(self, prompted, kicked):
+        res = set()
+        next_k = len(prompted[0]) + 1
+        for i, a in enumerate(prompted):
+            for b in prompted[i + 1:]:
+                merged = a | b
+                if len(merged) == next_k:
+                    ok = True
+                    for each in kicked:
+                        if each.issubset(merged):
+                            ok = False
+                            break
+                    if ok:
+                        res.add(merged)
+        return res
 
+    def apriori(self, min_support=0.5):
+        res = list()
+        candidate = self.one_itemset
+        while candidate:
+            prompted, kicked = self.filter_supported(candidate, min_support)
+            print('{}\n{}\n{}\n\n'.format(candidate, prompted, kicked))
+            if not prompted:
+                break
+            res.append(prompted)
+            candidate = self.generate_next(prompted, kicked)
 
-def rules_from_set_item(freq_set, h, support_data, br1, min_conf=0.7):
-    m = len(h[0])   # m = 1
-    if len(freq_set) > m+1:  # > 2
-        hmp1 = generate_posterity(h, m+1)
-        hmp1 = calc_conf(freq_set, hmp1, support_data, br1, min_conf)
-        if len(hmp1) > 1:
-            rules_from_set_item(freq_set, hmp1, support_data, br1, min_conf)
+        return res
 
 
 if __name__ == '__main__':
-    a, b = apriori(load_data())
-    generate_rules(a, b, 0.1)
+    aaa = Apriori()
+    for iii in aaa.apriori():
+        print(iii)
